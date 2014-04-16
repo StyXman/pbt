1495f0fdbe24f3b454e5cd20c4f23de184562e3f
Marcos Dione <mdione@grulic.org.ar>
2014-04-16 21:30:50 +0200

    [+] so it finds the deps installed by pbt install.

--- pbt.sh
+++ pbt.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 pwd=$(pwd)
-# so import pbt works
-export PYTHONPATH="$pwd:$PYTHONPATH"
+# so import pbt works and it finds any dep installed by pbt install
+export PYTHONPATH="$pwd:$pwd/deps:$PYTHONPATH"
 # so it finds only the plugins from the source
 export PBT_PLUGINS_PATH="$pwd/plugins"
 
